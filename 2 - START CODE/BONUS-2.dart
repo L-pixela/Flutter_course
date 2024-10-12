b7a3605559fada84157927d3d0b722c03fb47271
L-pixela <122270958+L-pixela@users.noreply.github.com>
2024-10-12 11:28:53 +0700

    Updated Command

--- 2 - START CODE/BONUS-2.dart
+++ 2 - START CODE/BONUS-2.dart
@@ -4,14 +4,18 @@ class Bracket {
   Bracket(this.s);
   //Test String if it has bracket or not.
   bool testString(String s) {
+    // Declaration for tracking the brackets
     int round = 0, curly = 0, square = 0;
+    // Finding the brackets one by one
     for (int i = 0; i < s.length; i++) {
       String char = s[i];
+      // test if it has the round, curly, square bracket
       if (char == '(') {
         round++;
         brackets.add(1);
       } else if (char == ')') {
         round--;
+        //condition where we check if the bracket has its pair
         if (brackets.isEmpty || brackets.removeLast() != 1) return false;
       } else if (char == '{') {
         curly++;
@@ -25,7 +29,7 @@ class Bracket {
       } else if (char == ']') {
         square--;
         if (brackets.isEmpty || brackets.removeLast() != 3) return false;
-      }
+      } // Condition to check if there is any bracket that don't have its match
       if (round < 0 || curly < 0 || square < 0) return false;
     }
     return round == 0 && curly == 0 && square == 0;
@@ -33,7 +37,7 @@ class Bracket {
 }
 
 void main() {
-  String run = "[{good}]";
+  String run = "good";
 
   Bracket test = Bracket(run);
   if (test.testString(run) == true) {
